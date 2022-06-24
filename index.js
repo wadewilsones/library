fc084e999616171c28c062b412aff060bf6e75a7
wadewilsones <wadewilsones@gmail.com>
2022-06-24 16:57:05 -0400

    Mark as read/unread was added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ addBookBtn.addEventListener('click', addBook);
 
 window.onload = () => {
     setStorage();
-    displayBooks()
+    displayBooks();
 }
 
 //Prototype of a book
@@ -17,7 +17,9 @@ class Books {
     constructor(name, author){
         this.bookName = name;
         this.author = author;
+        this.status = 'Unread';
     }
+
 }
 
 
@@ -25,42 +27,59 @@ class Books {
 
 function addBook(event){
     event.preventDefault();
-    let bookName = document.getElementById('bookName').value;
-    let bookAuthor = document.getElementById('author_name').value;
+    let bookName = document.getElementById('bookName');
+    let bookAuthor = document.getElementById('author_name');
+
     //Validate input
-    if(bookName != '' && bookAuthor != ''){
+    if(bookName.value != '' && bookAuthor.value != ''){
+
+        bookName.removeAttribute('class', 'Wrong-input');
+        bookName.setAttribute('class', 'Bookinput');
+
+        bookAuthor.removeAttribute('class', 'Wrong-input');
+        bookAuthor.setAttribute('class', 'Bookinput');
+
         //Create a new instance of a class
-        let newBook = new Books(bookName, bookAuthor)
+        let newBook = new Books(bookName.value, bookAuthor.value)
         //Push to array & add to Storage
         favoriteBooks.push(newBook);
         localStorage.setItem('BookList', JSON.stringify(favoriteBooks));
         //Display and Refresh
         cleanUI();
         displayBooks();
+        bookName.value = '';
+        bookAuthor.value = '';
     }
+    // Handle wron input
+    else {
 
-    else{
-        //Handle wrong input
-        let bookName = document.getElementById('bookName');
-        let bookAuthor = document.getElementById('author_name');
+        //If author field is empty
+        if (bookName.value != '' && bookAuthor.value == ''){
 
-        //Change style of input
-        bookName.removeAttribute('class', 'Wrong-input');
-        bookAuthor.setAttribute('class', 'Wrong-input');
-        bookName.setAttribute('class', 'Wrong-input');
-        bookAuthor.setAttribute('class', 'Wrong-input');
+            bookAuthor.removeAttribute('class', 'Bookinput'); // new style was applied
+            bookAuthor.setAttribute('class', 'Wrong-input');
 
-        //Change value
-        bookName.value = 'Your forgot a book name!';
-        bookAuthor.value = 'No author?';
+            bookAuthor.placeholder = 'Author name required!';
+        }
 
-        setTimeout( () => {
-            bookName.value = '';
-            bookAuthor.value = '';
-        }, 2000)
+        //If book name field is empty
+        else if (bookName.value == '' && bookAuthor.value != ''){
+
+            bookName.removeAttribute('class', 'Bookinput'); // new style was applied
+            bookName.setAttribute('class', 'Wrong-input');
+            bookName.placeholder = 'Book name required!';
+        }
+
+            bookName.removeAttribute('class', 'Bookinput');
+            bookName.setAttribute('class', 'Wrong-input');
+
+            bookAuthor.removeAttribute('class', 'Bookinput');
+            bookAuthor.setAttribute('class', 'Wrong-input');
+            //Change placeholder for inputs
+            bookName.placeholder = 'Book name required!';
+            bookAuthor.placeholder = 'Author name required!';
+        }
     }
-
-}
 
 
 //Add to local Storage
@@ -74,23 +93,49 @@ function setStorage(){
 
 }
 
-
 /*Display books */
 
 function displayBooks(){
 
     if(favoriteBooks.length > 0){
+
         favoriteBooks.forEach(element => {
-            let bookId = element.bookName;
             let book = document.createElement('div');
             book.innerHTML = `
+            <i class="fa fa-times removeBtn" aria-hidden="true" id = "${element.bookName}" onClick = "removeBook(event)"></i>
             <h3>${element.bookName}</h3>
-            <h4>By: ${element.author}</h4>
-            <button class = 'removeBtn' id = "${element.bookName}" onClick = "removeBook(event)">Remove book</button>`;
+            <h4>By: ${element.author}</h4>`
+            if(element.status == 'none'){
+                book.innerHTML += `<button class = 'readStatusBtn' id = "r-${element.bookName}"onClick = "markRead(event)">Mark as Read</button>`;
+            }
+            else{
+                book.innerHTML += `<button class = 'readStatusBtn' id = "r-${element.bookName}"onClick = "markRead(event)">Mark as Unread</button>`;
+            }
+           
             bookContainer.append(book);
         });
 
     }
+}
+
+/*Mark book as read*/
+
+function markRead(e){
+    let book = e.target.getAttribute('id');
+    book = book.slice(2,(book.length)); // get book-name part from id
+    let newArray = favoriteBooks.filter(element => element.bookName == book);
+
+    if(newArray[0].status != 'read'){
+        newArray[0].status = 'read';
+        localStorage.setItem('BookList', JSON.stringify(favoriteBooks));
+    }
+    else{
+        newArray[0].status = 'none';
+        localStorage.setItem('BookList', JSON.stringify(favoriteBooks));
+    }
+    cleanUI();
+    displayBooks();
+
 }
 
 /*Remove book*/
@@ -101,11 +146,12 @@ function removeBook(e){
     //Find a book in favorites books
     let newBookList = favoriteBooks.filter(element => element.bookName != book)
     favoriteBooks = newBookList;
-    console.log(favoriteBooks);
     localStorage.setItem('BookList', JSON.stringify(favoriteBooks));
     cleanUI();
     displayBooks();
 }
+
+
 
 /*Clean UI */
 
