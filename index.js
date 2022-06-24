837fb63da85a0c553c1697896d837e439b9012ac
wadewilsones <wadewilsones@gmail.com>
2022-06-24 14:48:47 -0400

    Remove btn was created

--- index.js
+++ index.js
@@ -25,17 +25,41 @@ class Books {
 
 function addBook(event){
     event.preventDefault();
+    let bookName = document.getElementById('bookName').value;
+    let bookAuthor = document.getElementById('author_name').value;
+    //Validate input
+    if(bookName != '' && bookAuthor != ''){
+        //Create a new instance of a class
+        let newBook = new Books(bookName, bookAuthor)
+        //Push to array & add to Storage
+        favoriteBooks.push(newBook);
+        localStorage.setItem('BookList', JSON.stringify(favoriteBooks));
+        //Display and Refresh
+        cleanUI();
+        displayBooks();
+    }
 
-    const bookName = document.getElementById('bookName').value;
-    const bookAuthor = document.getElementById('author_name').value;
-    //Create a new instance of a class
-    let newBook = new Books(bookName, bookAuthor)
-    //Push to array & add to Storage
-    favoriteBooks.push(newBook);
-    localStorage.setItem('BookList', JSON.stringify(favoriteBooks));
-    //Display and Refresh
-    cleanUI();
-    displayBooks();
+    else{
+        //Handle wrong input
+        let bookName = document.getElementById('bookName');
+        let bookAuthor = document.getElementById('author_name');
+
+        //Change style of input
+        bookName.removeAttribute('class', 'Wrong-input');
+        bookAuthor.setAttribute('class', 'Wrong-input');
+        bookName.setAttribute('class', 'Wrong-input');
+        bookAuthor.setAttribute('class', 'Wrong-input');
+
+        //Change value
+        bookName.value = 'Your forgot a book name!';
+        bookAuthor.value = 'No author?';
+
+        setTimeout( () => {
+            bookName.value = '';
+            bookAuthor.value = '';
+        }, 2000)
+    }
+
 }
 
 
@@ -45,7 +69,6 @@ function setStorage(){
     let dataFromStorage = window.localStorage.getItem('BookList');
     if(dataFromStorage != []){
         favoriteBooks = JSON.parse(dataFromStorage);
-        console.log(favoriteBooks);
     }
 
 
@@ -58,15 +81,30 @@ function displayBooks(){
 
     if(favoriteBooks.length > 0){
         favoriteBooks.forEach(element => {
+            let bookId = element.bookName;
             let book = document.createElement('div');
             book.innerHTML = `
-            <h3>Book Title: ${element.bookName}</h3>
-            <h4>Author:${element.author}</h4>
-            <button id = 'removeBook'>Remove book</button>`;
+            <h3>${element.bookName}</h3>
+            <h4>By: ${element.author}</h4>
+            <button class = 'removeBtn' id = "${element.bookName}" onClick = "removeBook(event)">Remove book</button>`;
             bookContainer.append(book);
         });
 
     }
+}
+
+/*Remove book*/
+
+
+function removeBook(e){
+    let book = e.target.getAttribute('id');
+    //Find a book in favorites books
+    let newBookList = favoriteBooks.filter(element => element.bookName != book)
+    favoriteBooks = newBookList;
+    console.log(favoriteBooks);
+    localStorage.setItem('BookList', JSON.stringify(favoriteBooks));
+    cleanUI();
+    displayBooks();
 }
 
 /*Clean UI */
